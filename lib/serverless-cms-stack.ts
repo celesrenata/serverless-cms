624a95d73e506aad4d7dab861e89c9ac58683cc9
Celes Renata <celes@frameshift.net>
2026-02-14 04:26:00 -0800

    fix: revert to creating S3 buckets instead of importing
    - Changed back to creating buckets with 'new s3.Bucket()'
    - The buckets don't exist yet, so importing them fails
    - Using RETAIN removal policy to prevent accidental deletion
    - This allows CloudFormation to create the buckets properly

--- lib/serverless-cms-stack.ts
+++ lib/serverless-cms-stack.ts
@@ -151,31 +151,61 @@ export class ServerlessCmsStack extends cdk.Stack {
     });
 
     // S3 Buckets
-    // Import existing buckets instead of creating new ones
-    const mediaBucketName = `cms-media-${props.environment}-${this.account}`;
-    const adminBucketName = `cms-admin-${props.environment}-${this.account}`;
-    const publicBucketName = `cms-public-${props.environment}-${this.account}`;
 
     // Media Bucket - for uploaded files and generated thumbnails
-    // Use fromBucketAttributes to reference existing bucket non-destructively
-    this.mediaBucket = s3.Bucket.fromBucketAttributes(this, 'MediaBucket', {
-      bucketName: mediaBucketName,
-      bucketArn: `arn:aws:s3:::${mediaBucketName}`,
-    }) as s3.Bucket;
+    this.mediaBucket = new s3.Bucket(this, 'MediaBucket', {
+      bucketName: `cms-media-${props.environment}-${this.account}`,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      autoDeleteObjects: false,
+      versioned: true,
+      cors: [
+        {
+          allowedMethods: [
+            s3.HttpMethods.GET,
+            s3.HttpMethods.POST,
+            s3.HttpMethods.PUT,
+            s3.HttpMethods.DELETE,
+          ],
+          allowedOrigins: ['*'], // Should be restricted to specific domains in production
+          allowedHeaders: ['*'],
+          exposedHeaders: ['ETag'],
+          maxAge: 3000,
+        },
+      ],
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+    });
+
+    // Add bucket policy to allow public read access to media files
+    this.mediaBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        principals: [new iam.AnyPrincipal()],
+        actions: ['s3:GetObject'],
+        resources: [`${this.mediaBucket.bucketArn}/*`],
+      })
+    );
 
     // Admin Panel Bucket - for hosting the React admin application
-    this.adminBucket = s3.Bucket.fromBucketAttributes(this, 'AdminBucket', {
-      bucketName: adminBucketName,
-      bucketArn: `arn:aws:s3:::${adminBucketName}`,
-      bucketWebsiteUrl: `http://${adminBucketName}.s3-website-${this.region}.amazonaws.com`,
-    }) as s3.Bucket;
+    this.adminBucket = new s3.Bucket(this, 'AdminBucket', {
+      bucketName: `cms-admin-${props.environment}-${this.account}`,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      autoDeleteObjects: false,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html', // SPA routing support
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+    });
 
     // Public Website Bucket - for hosting the React public website
-    this.publicBucket = s3.Bucket.fromBucketAttributes(this, 'PublicBucket', {
-      bucketName: publicBucketName,
-      bucketArn: `arn:aws:s3:::${publicBucketName}`,
-      bucketWebsiteUrl: `http://${publicBucketName}.s3-website-${this.region}.amazonaws.com`,
-    }) as s3.Bucket;
+    this.publicBucket = new s3.Bucket(this, 'PublicBucket', {
+      bucketName: `cms-public-${props.environment}-${this.account}`,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      autoDeleteObjects: false,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html', // SPA routing support
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+    });
 
     // Cognito User Pool for authentication
     this.userPool = new cognito.UserPool(this, 'UserPool', {
