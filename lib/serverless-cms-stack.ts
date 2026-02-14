c03d70bfb86a7623017eb887f2ed0102a35ed4f5
Celes Renata <celes@frameshift.net>
2026-02-14 14:50:39 -0800

    Revert certificate to DnsValidatedCertificate (CloudFront requires us-east-1)

--- lib/serverless-cms-stack.ts
+++ lib/serverless-cms-stack.ts
@@ -906,10 +906,11 @@ export class ServerlessCmsStack extends cdk.Stack {
         certDomains.push(`admin.${props.domainName}`);
       }
       
-      this.certificate = new acm.Certificate(this, 'Certificate', {
+      this.certificate = new acm.DnsValidatedCertificate(this, 'Certificate', {
         domainName: certDomains[0],
         subjectAlternativeNames: certDomains.slice(1),
-        validation: acm.CertificateValidation.fromDns(this.hostedZone),
+        hostedZone: this.hostedZone,
+        region: 'us-east-1', // CloudFront requires certificates in us-east-1
       });
     }
 
